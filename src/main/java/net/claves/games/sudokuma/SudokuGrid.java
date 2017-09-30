76d61a6db52716ab5140c0432dbd6e2e7cfe29a0
focoma <focoma@gmail.com>
2017-09-30 23:58:58 +0800

    Implemented grid regions

--- src/main/java/net/claves/games/sudokuma/SudokuGrid.java
+++ src/main/java/net/claves/games/sudokuma/SudokuGrid.java
@@ -3,96 +3,120 @@ package net.claves.games.sudokuma;
 import net.claves.games.Position;
 import net.claves.games.PositionsGenerator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class SudokuGrid {
-    private List<List<GridItem>> itemsByRow;
-    private List<List<GridItem>> itemsByColumn;
-//    private List<List<GridItem>> itemsBySquare;
+    private GridItem[][] itemsByRow;
+    private GridItem[][] itemsByColumn;
+    private GridItem[][] itemsByReqion;
+    private Map<Position, GridItem> gridMap;
+    private int size;
+    private int regionSize;
 
-    private SudokuGrid() {
+    public SudokuGrid(int size) {
+        if (size < 1)
+        {
+            throw new IllegalArgumentException("Invalid size.");
+        }
+        this.size = size;
+        int sqrt = (int)Math.sqrt(size);
+        if (sqrt * sqrt == size)
+        {
+            regionSize = sqrt;
+        }
+        itemsByRow = new GridItem[size][size];
+        itemsByColumn = new GridItem[size][size];
+        if (regionSize > 0) {
+            itemsByReqion = new GridItem[size][size];
+        }
 
+        gridMap = new HashMap<Position, GridItem>() {
+            @Override
+            public GridItem put(Position position, GridItem gridItem) {
+                if (position.x >= size || position.y >= size || position.x < 0 || position.y < 0)
+                {
+                    throw new IllegalArgumentException("Invalid position.");
+                }
+                itemsByRow[position.x][position.y] = gridItem;
+                itemsByColumn[position.y][position.x] = gridItem;
+                if (regionSize > 0)
+                {
+                    getRegion(position)
+                            [(position.x % regionSize)*regionSize + (position.y % regionSize)] = gridItem;
+                }
+
+                return super.put(position, gridItem);
+            }
+
+            @Override
+            public GridItem remove(Object key) {
+                Position position = (Position)key;
+                GridItem blank = new VariableItem();
+                put(position, blank);
+                return blank;
+            }
+        };
+    }
+
+    private GridItem[] getRegion(Position position) {
+        return itemsByReqion
+                [(position.x / regionSize)*regionSize + (position.y / regionSize)];
     }
 
     public static SudokuGrid newInstance() {
-        SudokuGrid sudokuGrid = new SudokuGrid();
+        SudokuGrid sudokuGrid = new SudokuGrid(9);
         sudokuGrid.generate();
 
         return sudokuGrid;
     }
 
-    public static SudokuGrid newInstance(Integer[][] grid) {
-        SudokuGrid sudokuGrid = new SudokuGrid();
-        sudokuGrid.parseArrayGrid(grid);
+    public static SudokuGrid newInstance(Integer[][] squareGrid) {
+        int size = squareGrid.length;
+        if (size != squareGrid[0].length)
+        {
+            throw new IllegalArgumentException("The grid must be a square.");
+        }
+        SudokuGrid sudokuGrid = new SudokuGrid(size);
+        sudokuGrid.parseArrayGrid(squareGrid);
 
         return sudokuGrid;
     }
 
     private void parseArrayGrid(Integer[][] grid) {
-        if (grid.length != 9 || grid[0].length != 9) {
-            throw new UnsupportedOperationException("Sudokuma currently supports 9x9 grids only.");
-        }
-
-        itemsByRow = new ArrayList<>();
-        itemsByColumn = new ArrayList<>();
         for (int rowIndex = 0; rowIndex < grid.length; rowIndex++) {
-            List<GridItem> row = new ArrayList<>();
-            itemsByRow.add(row);
             for (int columnIndex = 0; columnIndex < grid[rowIndex].length; columnIndex++) {
                 Integer value = grid[rowIndex][columnIndex];
-                if (value != null && (value < 1 || value > 9)) {
-                    throw new IllegalArgumentException("Values can only be from 1 to 9.");
+                if (value != null && (value < 1 || value > size)) {
+                    throw new IllegalArgumentException("Values can only be from 1 to " + size + ".");
                 }
 
-                List<GridItem> column;
-                if (rowIndex == 0) {
-                    column = new ArrayList<>();
-                    itemsByColumn.add(column);
-                } else {
-                    column = itemsByColumn.get(columnIndex);
-                }
-
-                GridItem gridItem = new GridItem(value);
-
-                row.add(gridItem);
-                column.add(gridItem);
+                gridMap.put(new Position(rowIndex, columnIndex), new GivenItem(value));
             }
         }
     }
 
     private void generate() {
-        itemsByRow = new ArrayList<>();
-        itemsByColumn = new ArrayList<>();
-
+        Collection<Position> givenPositions = getPositionsGenerator().generate();
         for (int rowIndex = 0; rowIndex < 9; rowIndex++) {
-            List<GridItem> row = new ArrayList<>();
-            itemsByRow.add(row);
             for (int columnIndex = 0; columnIndex < 9; columnIndex++) {
-                List<GridItem> column;
-                if (rowIndex == 0) {
-                    column = new ArrayList<>();
-                    itemsByColumn.add(column);
-                } else {
-                    column = itemsByColumn.get(columnIndex);
+                Integer randomValue = generateValueFor(new Position(rowIndex, columnIndex));
+                GridItem gridItem;
+                if (givenPositions.contains(new Position(rowIndex, columnIndex))) {
+                    gridItem = new GivenItem(randomValue);
+                }
+                else {
+                    gridItem = new VariableItem();
+                    gridItem.setValue(randomValue);
                 }
 
-                GridItem gridItem = new GridItem(generateValueFor(new Position(rowIndex, columnIndex)));
-
-                row.add(gridItem);
-                column.add(gridItem);
+                gridMap.put(new Position(rowIndex, columnIndex), gridItem);
             }
         }
 
-        Collection<Position> positionsToPopulate = getPositionsGenerator().generate();
         for (int rowIndex = 0; rowIndex < 9; rowIndex++) {
             for (int columnIndex = 0; columnIndex < 9; columnIndex++) {
                 GridItem gridItem = getGridItem(rowIndex, columnIndex);
-                if (positionsToPopulate.contains(new Position(rowIndex, columnIndex))) {
-                    gridItem.setFinal();
-                } else {
+                if (!givenPositions.contains(new Position(rowIndex, columnIndex))) {
                     gridItem.setValue(null);
                 }
             }
@@ -114,32 +138,43 @@ public class SudokuGrid {
 
     protected List<Integer> getValidValuesFor(Position position) {
         List<Integer> validValues = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
-        for (GridItem gridItem : itemsByRow.get(position.x)) {
-            validValues.remove(gridItem.getValue());
+        for (GridItem gridItem : itemsByRow[position.x]) {
+            if (gridItem != null) {
+                validValues.remove(gridItem.getValue());
+            }
         }
-        for (GridItem gridItem : itemsByColumn.get(position.y)) {
-            validValues.remove(gridItem.getValue());
+        for (GridItem gridItem : itemsByColumn[position.y]) {
+            if (gridItem != null) {
+                validValues.remove(gridItem.getValue());
+            }
+        }
+        if (regionSize > 0) {
+            for (GridItem gridItem : getRegion(position)) {
+                if (gridItem != null) {
+                    validValues.remove(gridItem.getValue());
+                }
+            }
         }
 
         return validValues;
     }
 
     public int getItem(int row, int column) {
-        return itemsByRow.get(row).get(column).value;
+        return getGridItem(row, column).value;
     }
 
     public GridItem getGridItem(int row, int column) {
-        return itemsByRow.get(row).get(column);
+        return itemsByRow[row][column];
     }
 
     public boolean isValid() {
-        for (List<GridItem> row : itemsByRow) {
+        for (GridItem[] row : itemsByRow) {
             if (!itemsAreUnique(row)) {
                 return false;
             }
         }
 
-        for (List<GridItem> column : itemsByColumn) {
+        for (GridItem[] column : itemsByColumn) {
             if (!itemsAreUnique(column)) {
                 return false;
             }
@@ -150,7 +185,7 @@ public class SudokuGrid {
 
     public boolean hasEnoughGivens() {
         int givenCount = 0;
-        for (List<GridItem> row : itemsByRow) {
+        for (GridItem[] row : itemsByRow) {
             for (GridItem gridItem : row) {
                 if (gridItem.getValue() != null) {
                     givenCount++;
@@ -161,20 +196,20 @@ public class SudokuGrid {
         return givenCount > 16;
     }
 
-    private boolean itemsAreUnique(List<GridItem> row) {
+    private boolean itemsAreUnique(GridItem[] row) {
         List<Integer> processedValues = new ArrayList<>();
         for (GridItem gridItem : row) {
             if (gridItem.getValue() != null && processedValues.contains(gridItem.getValue())) {
                 return false;
             }
+            processedValues.add(gridItem.getValue());
         }
 
         return true;
     }
 
-    private class GridItem {
+    private abstract class GridItem {
         private Integer value;
-        private boolean isFinal;
 
         public GridItem(Integer value) {
             this.value = value;
@@ -184,15 +219,8 @@ public class SudokuGrid {
             return value;
         }
 
-        public void setValue(Integer value) throws IllegalAccessError {
-            if (isFinal) {
-                throw new IllegalAccessError();
-            }
+        public void setValue(Integer value) {
             this.value = value;
-        }
-
-        void setFinal() {
-            this.isFinal = true;
         }
 
         @Override
@@ -215,4 +243,31 @@ public class SudokuGrid {
         }
     }
 
+    private class GivenItem extends GridItem
+    {
+        public GivenItem(Integer value)
+        {
+            super(value);
+        }
+    }
+
+    private class VariableItem extends GridItem
+    {
+        private Set<Integer> possibilities;
+
+        public VariableItem()
+        {
+            super(null);
+            possibilities = new HashSet<>();
+            for (int i = 1; i <= size; i++)
+            {
+                possibilities.add(i);
+            }
+        }
+
+        public void removePossibility(Integer possibility)
+        {
+            possibilities.remove(possibility);
+        }
+    }
 }
